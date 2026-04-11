ccf524e9ce5e81e20eb6419e13c66ba5df480b41
kaioman <kajin0318@gmail.com>
2026-04-11 10:39:58 +0900

    app_config.jsonからgpcの設定をgcp-config.jsonに移行

--- unchain-aiaw/app/test.py
+++ unchain-aiaw/app/test.py
@@ -2,11 +2,17 @@ import os
 import uwxpy.configs.app_init as app
 
 # アプリ初期化
-app.init_app(__file__, "app_config.json", os.environ.get('CONFIG_FILE_NAME', 'unchain-aiaw-dev.json'))
+app.init_app(
+    __file__, 
+    "app_config.json", 
+    os.environ.get('GCP_CONFIG_FILE_NAME', 'gcp_config-dev.json'),
+    os.environ.get('CONFIG_FILE_NAME', 'unchain-aiaw-dev.json')
+)
 
 # 環境変数テスト
 print(os.environ.get('PROJECT_ROOT', 'root not found'))
 print(os.environ.get('CONFIG_FILE_NAME', 'config not found'))
+print(os.environ.get('GCP_CONFIG_FILE_NAME', 'gcp config not found'))
 
 # configテスト
 print(app.core.config.uwgen.endpoint)
